80708d4117a1f6a9fcb19656390c13a23b111b85
Alexis Roda <alexis.roda.villalonga@gmail.com>
2019-07-13 12:46:35 +0200

    Code refactor and cleanup.

--- src/main.c
+++ src/main.c
@@ -7,17 +7,55 @@
 #include <cpctelera.h>
 #include <stdio.h>
 
+/**************************************************************************/
+/* MACROS                                                                 */
+/**************************************************************************/
+
+#define CMD_NONE 0
+#define CMD_ATTACK 1
+#define CMD_DEFEND 2
+
+
+/**************************************************************************/
+/* TYPES                                                                  */
+/**************************************************************************/
+
+typedef u8 command_t;
+
+typedef struct {
+   u8 energy;
+   u8 attack;
+   u8 defense;
+   const char *name;
+} entity_t;
+
+
+/**************************************************************************/
+/* PROTOTYPES                                                             */
+/**************************************************************************/
+
+void init_globals(void);
+void display_stats(entity_t *entity);
+command_t get_player_command(void);
+command_t get_enemy_command(void);
+void execute_turn(command_t command, entity_t *me, entity_t *opponent);
+
+
+/**************************************************************************/
+/* GLOBALS                                                                */
+/**************************************************************************/
+
+entity_t player;
+entity_t enemy;
+
+
+/**************************************************************************/
+/* MAIN                                                                   */
+/**************************************************************************/
 
 void main(void) {
-    // Init variables
-    u8 energy    = 100;
-    u8 attack    = 30;
-    u8 defense   = 15;
-    u8 energyen  = 90;
-    u8 attacken  = 20;
-    u8 defenseen = 10;
 
-    cpct_keyID key_pressed = 0;
+    init_globals();
 
     // Let's start!
     printf("RPG GAME\r\n");
@@ -30,41 +68,76 @@ void main(void) {
     }
 
     while (1) {
-
         // CLS
         putchar(12);
 
-        // Print stats
-        printf("PLAYER [%d] (a%d) (d%d)\r\n", energy,   attack,   defense);
-        printf("ENEMY  [%d] (a%d) (d%d)\r\n", energyen, attacken, defenseen);
+        display_stats(&player);
+        display_stats(&enemy);
 
-        key_pressed = 0;
-        do {
-            cpct_scanKeyboard();
-            if (cpct_isKeyPressed(Key_A)) {
-                key_pressed = Key_A;
-            } else if (cpct_isKeyPressed(Key_D)) {
-                key_pressed = Key_D;
-            }
-        } while (key_pressed == 0);
+        execute_turn(get_player_command(), &player, &enemy);
+        execute_turn(get_enemy_command(), &enemy, &player);
+    }
+}
 
-        switch (key_pressed) {
-        case Key_A:
-            // PLAYER ATTACKS!
-            energyen -= attack;
-            break;
 
-        case Key_D:
-            energy += defense;
-            break;
+/**************************************************************************/
+/* FUNCTIONS                                                              */
+/**************************************************************************/
+
+void init_globals(void) {
+    player.energy = 100;
+    player.attack = 30;
+    player.defense = 15;
+    player.name = "PLAYER";
+
+    enemy.energy = 90;
+    enemy.attack = 20;
+    enemy.defense = 10;
+    enemy.name = "ENEMY";
+}
+
+void display_stats(entity_t *entity) {
+    printf("%s [%d] (a%d) (d%d)\r\n",
+           entity->name,
+           entity->energy,
+           entity->attack,
+           entity->defense
+           );
+}
+
+command_t get_player_command(void) {
+    command_t command;
+
+    command = CMD_NONE;
+    do {
+        cpct_scanKeyboard();
+        if (cpct_isKeyPressed(Key_A)) {
+            command = CMD_ATTACK;
+        } else if (cpct_isKeyPressed(Key_D)) {
+            command = CMD_DEFEND;
         }
+    } while (command == CMD_NONE);
 
-        // ENEMY DECIDE
-        if (cpct_rand() < 64) {
-            energyen += defenseen;
-        } else {
-            energy -= attacken;
-        }
+    return command;
+}
+
+command_t get_enemy_command(void) {
+    if (cpct_rand() < 64) {
+        return CMD_DEFEND;
+    } else {
+        return CMD_ATTACK;
+    }
+}
+
+void execute_turn(command_t command, entity_t *me, entity_t *opponent) {
+    switch (command) {
+    case CMD_ATTACK:
+        opponent->energy -= me->attack;
+        break;
+
+    case CMD_DEFEND:
+        me->energy += me->defense;
+        break;
     }
 
 }
